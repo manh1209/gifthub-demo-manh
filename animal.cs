c5ab2ca01df625a2fedac28a926060b8d566fa88
manh <trinhminhanh5794@gmail.com>
2022-07-11 22:45:10 +0700

    update

---
+++ animal.cs
@@ -0,0 +1,18 @@
+using (System)
+using (resource)
+{
+    resource
+}
+{
+    
+}
+
+namespace  WebApi.Entities
+public class DataContext : DbContext
+{
+    public DbSet<Account> { get; set; }
+    p
+}
+{
+    
+}
---
+++ area.cs
@@ -0,0 +1,19 @@
+using System;
+namespace oop
+{
+    internal class geometry
+    {
+        public geometry()
+        {
+            
+        }
+        public float a;
+        public float b;
+
+        public float area()
+        
+    }
+}
+{
+    
+}
